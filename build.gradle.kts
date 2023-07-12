fb82430ecf5ecd042215bc118f997116af6d3b36
VasanthaKumar Annadurai <vasanthakumar.annadurai@freshworks.com>
2023-07-12 06:49:09 +0530

    Added - Gradle Plugin Publish.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
+    alias(libs.plugins.gradlePluginPublish)
 }
 
 group = "com.vasanth"
-version = "1.0-SNAPSHOT"
+version = "0.1.0-alpha.1"
 
 repositories {
     mavenCentral()
@@ -13,16 +14,33 @@ repositories {
 }
 
 gradlePlugin {
+    website.set("https://github.com/apvasanth03/jacoco-android-gradle-plugin")
+    vcsUrl.set("https://github.com/apvasanth03/jacoco-android-gradle-plugin")
+
     plugins {
-        create("JacocoAndroidPlugin") {
+        create("jacocoAndroidPlugin") {
             id = "com.vasanth.jacoco-android"
+            displayName = "JaCoCo Android Plugin"
+            description = "The JaCoCoAndroid plugin provides code coverage metrics for single-module Android project."
+            tags.set(listOf("jacoco", "jacocoandroid", "codecoverage", "android"))
             implementationClass = "com.vasanth.jacocoandroid.JacocoAndroidPlugin"
         }
-        create("JacocoReportAggregationAndroidPlugin") {
+        create("jacocoReportAggregationAndroidPlugin") {
             id = "com.vasanth.jacoco-report-aggregation-android"
+            displayName = "Jacoco Report Aggregation Android Plugin"
+            description =
+                "The JacocoReportAggregationAndroidPlugin plugin provides code coverage metrics for multi-module Android project."
+            tags.set(listOf("jacoco", "jacocoandroid", "codecoverage", "android"))
             implementationClass = "com.vasanth.jacocoandroid.JacocoReportAggregationAndroidPlugin"
         }
     }
+}
+
+if ((System.getenv("GRADLE_PLUGIN_PUBLISH_KEY") != null) &&
+    (System.getenv("GRADLE_PLUGIN_PUBLISH_SECRET") != null)
+) {
+    project.ext["gradle.publish.key"] = System.getenv("GRADLE_PLUGIN_PUBLISH_KEY")
+    project.ext["gradle.publish.secret"] = System.getenv("GRADLE_PLUGIN_PUBLISH_SECRET")
 }
 
 dependencies {
